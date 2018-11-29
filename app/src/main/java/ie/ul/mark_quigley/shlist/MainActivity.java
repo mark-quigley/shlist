8837ec61426d34eae7d9e12fac3d0656d57b80b0
trysalot <16230833@studentmail.ul.ie>
2018-11-29 23:19:04 +0000

    R is gone to god

--- app/src/main/java/ie/ul/mark_quigley/shlist/MainActivity.java
+++ app/src/main/java/ie/ul/mark_quigley/shlist/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;

3672dfa66441d2ba2a4049f2fe08dc6e37eddcf2
trysalot <16230833@studentmail.ul.ie>
2018-11-29 19:35:47 +0000

    Actitivty screen added, no action yet.

---
+++ app/src/main/java/ie/ul/mark_quigley/shlist/AldiListDetailActivity.java
@@ -0,0 +1,16 @@
+package ie.ul.mark_quigley.shlist;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class AldiListDetailActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aldi_list_detail);
+    }
+
+
+
+}

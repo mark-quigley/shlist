efb4a5e94451fb2290e2fc166c4786224bcb1479
trysalot <16230833@studentmail.ul.ie>
2018-11-29 21:00:18 +0000

    change shlist to Shopping List

--- app/src/main/java/ie/ul/mark_quigley/shlist/MainActivity.java
+++ app/src/main/java/ie/ul/mark_quigley/shlist/MainActivity.java
@@ -42,6 +42,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Toolbar
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
